aa048e70fd572dc8a7ff853081b284309cebde9f
Tatiane Pereira Bonfim <tpbonfim2002@gmail.com>
2019-07-23 22:31:24 -0300

    implementado inicvio de adicao de endereco

---
+++ src/app/add-endereco/add-endereco.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-endereco',
+  templateUrl: './add-endereco.page.html',
+  styleUrls: ['./add-endereco.page.scss'],
+})
+export class AddEnderecoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  buscar(cep){
+    const cepString = cep.el.value
+    const cepValido = cepString.replace('1', 'ola');
+    console.log(cepValido)
+  }
+}
