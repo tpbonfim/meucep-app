e8873fd8541d54d12f1cf9a02868b212bdc3cd1e
Tatiane Pereira Bonfim <tpbonfim2002@gmail.com>
2019-07-23 22:04:18 -0300

    Emplementado estrutura de listagem e classe de dominio emdereco

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import endereco from 'src/app/domain/endereco';
+import { NumericValueAccessor } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
+
+  enderecos: Array<object> = []
+
+  ionViewDidEnter() {
+    let endereco1 = new endereco()
+    endereco1.rua = 'Rua Cambara'
+    endereco1.bairro = 'Jardim Miray'
+    endereco1.cidade = 'Itaquaquecetuba'
+    endereco1.estado = 'Sp'
+    endereco1.numero = '866'
+
+    this.enderecos.push(endereco1)
+  }
 
 }
